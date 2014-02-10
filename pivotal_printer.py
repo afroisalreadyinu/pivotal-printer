a9dd2db8bada2309f5c4ad766816283ad6a96803
Ulas Tuerkmen <ulas@goodscloud.net>
2014-02-10 09:39:57 +0100

    Read api key from .pivotal_key, added estimated, created, current state.

--- pivotal_printer.py
+++ pivotal_printer.py
@@ -19,27 +19,51 @@ def generate_pdf(story, existing_canvas=None):
     #c.setFont("Helvetica", 28)
     # draw some lines
     #a5 is 210 mm * 148 mm
-    c.rect(1*cm, 1*cm, 12.8*cm, 19*cm, fill=0)
-
+    #a6 is 148 mm * 105 mm
+    c.rect(0.5*cm, 0.5*cm, 9.5*cm, 13.8*cm, fill=0)
     c.rotate(90)
-    style.fontSize = 24
-    style.leading = 24
-    story_points = ("%s points" % story['estimate']
+
+    style.fontSize = 12
+    style.leading = 12
+
+    story_points = ("Estimate: %s points" % story['estimate']
                     if ('estimate' in story and story['estimate'])
                     else 'Not estimated')
-    title = Paragraph("%s [%s]" % (story['name'],story_points), style)
+    points = Paragraph(story_points, style)
+    points.wrap(13*cm, 9*cm)
+    points.drawOn(c, 1*cm, -9*cm)
 
-    title.wrap(17*cm, 4*cm)
-    title.drawOn(c, 2*cm, -3.5*cm)
+
+    created_text = "Created on: %s" % story['created_at'].strftime("%d.%m.%Y %H:%M")
+    created = Paragraph(created_text, style)
+    created.wrap(13*cm, 9*cm)
+    created.drawOn(c, 1*cm, -8*cm)
+
+    status_text = "Status: %s" % story['current_state']
+    status = Paragraph(status_text, style)
+    status.wrap(13*cm, 9*cm)
+    status.drawOn(c, 1*cm, -7*cm)
+
+    style.fontSize = 14
+    style.leading = 16
+    description_text = story['description']
+    MAX_DESC = 200
+    if len(description_text) > MAX_DESC:
+        description_text = description_text[:MAX_DESC] + "..."
+    description = Paragraph(description_text, style)
+    description.wrap(13*cm, 9*cm)
+    description.drawOn(c, 1*cm, -6*cm)
+
+    style.fontSize = 18
+    style.leading = 18
+    title = Paragraph(story['name'], style)
+
+    title.wrap(13*cm, 2*cm)
+    title.drawOn(c, 1*cm, -2.5*cm)
 
     # c.drawString(2*cm, -3*cm, "%s [%d points]" % (story['Subject'],
     #                                               int(float(story['Story points']))))
 
-    style.fontSize = 14
-    style.leading = 16
-    description = Paragraph(story['description'], style)
-    description.wrap(14*cm, 15*cm)
-    description.drawOn(c, 2*cm, -8*cm)
     c.showPage()
     return c
 
@@ -59,17 +83,15 @@ def get_stories(api_token):
     return [x for x in stories if x['current_state'] != 'accepted']
 
 def print_stories():
-    if len(sys.argv) != 2:
-        print "Usage: print-stories API-KEY"
-        return
-    stories = get_stories(sys.argv[1])
+    with open(os.path.expanduser("~/.pivotal_key")) as key_file:
+        api_key = key_file.read().strip()
+    stories = get_stories(api_key)
     process_stories(stories)
 
 def save_stories():
-    if len(sys.argv) != 2:
-        print "Usage: save-stories API-KEY"
-        return
-    stories = get_stories(sys.argv[1])
+    with open(os.path.expanduser("~/.pivotal_key")) as key_file:
+        api_key = key_file.read().strip()
+    stories = get_stories(api_key)
     with open('stories.pickle', 'wb') as pickle_file:
         pickle_file.write(pickle.dumps(stories))
     print "Wrote stories to stories.pickle"
