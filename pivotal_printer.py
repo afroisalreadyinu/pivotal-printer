c68879959c121f12672d1e0aba3cc393af62fe7d
Ulas Tuerkmen <ulas@goodscloud.com>
2014-10-06 13:28:06 +0200

    Refactoring to allow different kinds of printing

--- pivotal_printer.py
+++ pivotal_printer.py
@@ -13,60 +13,68 @@ OUT_FILENAME = 'stories.pdf'
 MAX_DESC = 200
 PIVOTAL_KEY_FILE = "~/.pivotal_key"
 PICKLE_FILE = 'stories.pickle'
-def generate_pdf(story, existing_canvas=None):
-    styleSheet = getSampleStyleSheet()
-    style = styleSheet['BodyText']
 
-    c = existing_canvas or canvas.Canvas(OUT_FILENAME, pagesize=A6)
-    #c.setFont("Helvetica", 28)
-    # draw some lines
-    #a5 is 210 mm * 148 mm
-    #a6 is 148 mm * 105 mm
-    c.rect(0.5*cm, 0.5*cm, 9.5*cm, 13.8*cm, fill=0)
-    c.rotate(90)
+class PdfPrinter(object):
 
-    style.fontSize = 12
-    style.leading = 12
+    def __init__(self, stories, filename):
+        self.stories = stories
+        self.canvas = canvas.Canvas(filename, pagesize=A6)
 
-    story_points = ("Estimate: %s points" % story['estimate']
-                    if ('estimate' in story and story['estimate'])
-                    else 'Not estimated')
-    points = Paragraph(story_points, style)
-    points.wrap(13*cm, 9*cm)
-    points.drawOn(c, 1*cm, -9*cm)
+    def print_stories(self):
+        for story in self.stories:
+            self.print_story(story)
+
+    def print_story(self, story):
+        styleSheet = getSampleStyleSheet()
+        style = styleSheet['BodyText']
+
+        #self.canvas.setFont("Helvetica", 28)
+        # draw some lines
+        #a5 is 210 mm * 148 mm
+        #a6 is 148 mm * 105 mm
+        self.canvas.rect(0.5*cm, 0.5*cm, 9.5*cm, 13.8*cm, fill=0)
+        self.canvas.rotate(90)
+
+        style.fontSize = 12
+        style.leading = 12
+
+        story_points = ("Estimate: %s points" % story['estimate']
+                        if ('estimate' in story and story['estimate'])
+                        else 'Not estimated')
+        points = Paragraph(story_points, style)
+        points.wrap(13*cm, 9*cm)
+        points.drawOn(self.canvas, 1*cm, -9*cm)
 
 
-    created_text = "Created on: %s" % story['created_at'].strftime("%d.%m.%Y %H:%M")
-    created = Paragraph(created_text, style)
-    created.wrap(13*cm, 9*cm)
-    created.drawOn(c, 1*cm, -8*cm)
+        created_text = "Created on: %s" % story['created_at'].strftime("%d.%m.%Y %H:%M")
+        created = Paragraph(created_text, style)
+        created.wrap(13*cm, 9*cm)
+        created.drawOn(self.canvas, 1*cm, -8*cm)
 
-    status_text = "Status: %s" % story['current_state']
-    status = Paragraph(status_text, style)
-    status.wrap(13*cm, 9*cm)
-    status.drawOn(c, 1*cm, -7*cm)
+        status_text = "Status: %s" % story['current_state']
+        status = Paragraph(status_text, style)
+        status.wrap(13*cm, 9*cm)
+        status.drawOn(self.canvas, 1*cm, -7*cm)
 
-    style.fontSize = 16
-    style.leading = 16
-    id_text = "ID: %s" % story['id']
-    id_paragraph = Paragraph(id_text, style)
-    id_paragraph.wrap(13*cm, 9*cm)
-    id_paragraph.drawOn(c, 1*cm, -6*cm)
+        style.fontSize = 16
+        style.leading = 16
+        id_text = "ID: %s" % story['id']
+        id_paragraph = Paragraph(id_text, style)
+        id_paragraph.wrap(13*cm, 9*cm)
+        id_paragraph.drawOn(self.canvas, 1*cm, -6*cm)
 
-    style.fontSize = 24
-    style.leading = 26
-    title = Paragraph(story['name'], style)
-    title.wrap(13*cm, 2*cm)
-    title.drawOn(c, 1*cm, -4*cm)
+        style.fontSize = 24
+        style.leading = 26
+        title = Paragraph(story['name'], style)
+        title.wrap(13*cm, 2*cm)
+        title.drawOn(self.canvas, 1*cm, -4*cm)
 
-    c.showPage()
-    return c
+        self.canvas.showPage()
 
-def process_stories(stories):
-    canvas = None
-    for story in stories:
-        canvas = generate_pdf(story, canvas)
-    canvas.save()
+def process_stories(stories, print_format):
+    if print_format == 'pdf':
+        printer = PdfPrinter(stories, OUT_FILENAME)
+        printer.print_stories()
     print "Stories saved to %s" % OUT_FILENAME
 
 def get_stories(api_token):
@@ -81,7 +89,7 @@ def print_stories():
     with open(os.path.expanduser(PIVOTAL_KEY_FILE)) as key_file:
         api_key = key_file.read().strip()
     stories = get_stories(api_key)
-    process_stories(stories)
+    process_stories(stories, 'pdf')
 
 def save_stories():
     with open(os.path.expanduser(PIVOTAL_KEY_FILE)) as key_file:
