53db835668ecd686ce1defd750c61518140e52f6
Ulas Tuerkmen <ulas@goodscloud.com>
2014-10-06 14:51:33 +0200

    Added html printer

--- pivotal_printer.py
+++ pivotal_printer.py
@@ -7,12 +7,28 @@ from reportlab.lib.pagesizes import A6
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph
 
+import jinja2
+
 from busyflow.pivotal import PivotalClient
 
-OUT_FILENAME = 'stories.pdf'
+OUT_FILENAME = 'stories'
 MAX_DESC = 200
 PIVOTAL_KEY_FILE = "~/.pivotal_key"
 PICKLE_FILE = 'stories.pickle'
+
+class HtmlPrinter(object):
+
+    def __init__(self, stories, filename):
+        self.stories = stories
+        self.filename = filename
+
+    def print_stories(self):
+        template_path = os.path.abspath(os.path.join(__file__, "../stories.html.tmpl"))
+        with open(template_path, 'r') as template_file:
+            template = jinja2.Template(template_file.read())
+        rendered = template.render(stories=self.stories)
+        with open(self.filename, 'w') as outfile:
+            outfile.write(rendered)
 
 class PdfPrinter(object):
 
@@ -39,9 +55,7 @@ class PdfPrinter(object):
         style.fontSize = 12
         style.leading = 12
 
-        story_points = ("Estimate: %s points" % story['estimate']
-                        if ('estimate' in story and story['estimate'])
-                        else 'Not estimated')
+        story_points = "Estimate: %s points" % story['estimate']
         points = Paragraph(story_points, style)
         points.wrap(13*cm, 9*cm)
         points.drawOn(self.canvas, 1*cm, -9*cm)
@@ -73,24 +87,37 @@ class PdfPrinter(object):
         self.canvas.showPage()
 
 def process_stories(stories, print_format):
+    out_file = OUT_FILENAME + "." + print_format
     if print_format == 'pdf':
-        printer = PdfPrinter(stories, OUT_FILENAME)
+        printer = PdfPrinter(stories, out_file)
         printer.print_stories()
-    print "Stories saved to %s" % OUT_FILENAME
+    elif print_format == 'html':
+        printer = HtmlPrinter(stories, out_file)
+        printer.print_stories()
+    print "Stories saved to %s" % out_file
 
 def get_stories(api_token):
     client = PivotalClient(token=None, cache='/tmp')
     client.token = api_token
     projects = client.projects.all()['projects']
     iterations = client.iterations.current(projects[0]['id'])['iterations']
-    stories = iterations[0]['stories']
-    return [x for x in stories if x['current_state'] != 'accepted']
+    stories = [x for x in iterations[0]['stories'] if x['current_state'] != 'accepted']
+    index = 1
+    for story in stories:
+        story['estimate'] = story.get('estimate', 'Not estimated')
+        story['index'] = index
+        index += 1
+    return stories
+
+PRINT_FORMAT_ERROR = "Print format has to be one of pdf or html"
 
 def print_stories():
+    print_format = sys.argv[1] if len(sys.argv) > 1 else 'pdf'
+    assert print_format in ['pdf', 'html'], PRINT_FORMAT_ERROR
     with open(os.path.expanduser(PIVOTAL_KEY_FILE)) as key_file:
         api_key = key_file.read().strip()
     stories = get_stories(api_key)
-    process_stories(stories, 'pdf')
+    process_stories(stories, print_format)
 
 def save_stories():
     with open(os.path.expanduser(PIVOTAL_KEY_FILE)) as key_file:
@@ -101,6 +128,8 @@ def save_stories():
     print "Wrote stories to %s" % PICKLE_FILE
 
 def print_from_file():
+    print_format = sys.argv[1] if len(sys.argv) > 1 else 'pdf'
+    assert print_format in ['pdf', 'html'], PRINT_FORMAT_ERROR
     with open(PICKLE_FILE, 'rb') as pickle_file:
         stories = pickle.loads(pickle_file.read())
-    process_stories(stories)
+    process_stories(stories, print_format)
