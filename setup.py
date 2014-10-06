737ffeece4b0c0036b72ee08ef56f511bda2741b
Ulas Tuerkmen <ulas@goodscloud.com>
2014-10-06 14:50:59 +0200

    Added jinja2 as dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "pivotal-printer",
     version = "0.01",
     author = "Ulas Tuerkmen ",
-    install_requires = ["reportlab==2.6", "busyflow.pivotal==0.3.4", "httplib2"],
+    install_requires = ["reportlab==2.6", "busyflow.pivotal==0.3.4", "httplib2", 'jinja2'],
     packages=find_packages(),
     zip_safe=False,
     entry_points = {'console_scripts': ['print-stories = pivotal_printer:print_stories',
